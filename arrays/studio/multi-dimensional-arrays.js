728afd2f3fc1c57c589aeb8004be30ff7b3b3735
Chandralekha <chandralekha.murugan@gmail.com>
2024-01-18 23:42:17 -0600

    Class3-Studio-Strings-Arrays-Completed

--- arrays/studio/multi-dimensional-arrays.js
+++ arrays/studio/multi-dimensional-arrays.js
@@ -1,14 +1,42 @@
-let food = "water bottles,meal packs,snacks,chocolate";
-let equipment = "space suits,jet packs,tool belts,thermal detonators";
-let pets = "parrots,cats,moose,alien eggs";
-let sleepAids = "blankets,pillows,eyepatches,alarm clocks";
+const input = require('readline-sync');
+
+food = "water bottles,meal packs,snacks,chocolate";
+equipment = "space suits,jet packs,tool belts,thermal detonators";
+pets = "parrots,cats,moose,alien eggs";
+sleepAids = "blankets,pillows,eyepatches,alarm clocks";
 
 //1) Use split to convert the strings into four cabinet arrays. Alphabetize the contents of each cabinet.
+let cabinet1 = food.split(",").sort();
+let cabinet2 = equipment.split(",").sort();
+let cabinet3 = pets.split(",").sort();
+let cabinet4 = sleepAids.split(",").sort();
+console.log(cabinet1, cabinet2, cabinet3, cabinet4);
+
 
 //2) Initialize a cargoHold array and add the cabinet arrays to it. Print cargoHold to verify its structure.
+let cargoHold = [cabinet1, cabinet2, cabinet3, cabinet4];
+
+console.log(cargoHold);
 
 //3) Query the user to select a cabinet (0 - 3) in the cargoHold.
+let newcargohold = Number(input.question("Please select a cabinet in the CargoHold between (0 - 3): "));
 
 //4) Use bracket notation and a template literal to display the contents of the selected cabinet. If the user entered an invalid number, print an error message.
+if (newcargohold >= 0 && newcargohold < 4) {
+    console.log(`selected cabinet ${newcargohold}: ${cargoHold[newcargohold]}`);
+} else {
+    console.log("Error: Please select a cabinet number between 0-3 in the cargo_hold1")
+}
+    
 
 //5) Modify the code to query the user for BOTH a cabinet in cargoHold AND a particular item. Use the 'includes' method to check if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
+let particularitem = (input.question("Please enter a cabinet number in cargo to pick an particular item: "));
+if(newcargohold >= 0 && newcargohold < 4) {
+  if (cargoHold[newcargohold].includes(particularitem)){
+     console.log(`selected cabinet ${newcargohold} Does contain ${particularitem}.`);
+  } else {
+    console.log(`selected cabinet ${newcargohold} Does not contain ${particularitem}`);
+  }
+} else {
+console.log("Error: Please select a cabinet number between 0-3 in the cargo_hold1");
+}
--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -3,9 +3,29 @@ let str = "LaunchCode";
 
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
+let removestr = str.slice(0,3);
+console.log(removestr);
+let newstr = (str.slice(3) + removestr);
+// let newstr = (str.slice(3).push(removestr));
+console.log(newstr);
 
 //Use a template literal to print the original and modified string in a descriptive phrase.
+console.log(`Original String: ${str}`);
+console.log(`Modified String: ${newstr}`);
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
+let modifystr = (input.question("Enter the string: "));
+let swap = Number(input.question("Enter the number of letters to be swap?: "));
+let modifystr1 = modifystr.slice(swap) + modifystr.slice(0, swap);
+console.log(`Original String: ${modifystr}`);
+console.log(`Modified String: ${modifystr1}`);
 
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+let maxwordlength = (8);
+if (modifystr.length >= maxwordlength ) {
+  console.log("Error: User inputs are longer the word. Defaultly moving 3 characters");
+  modifystr = modifystr.slice(3);
+  console.log(modifystr);
+} else {
+console.log("Input is Valid");
+}
