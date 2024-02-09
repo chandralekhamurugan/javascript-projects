b6ae37c1ed1419ff26645fbb2fc9dc45838bf399
Chandralekha <chandralekha.murugan@gmail.com>
2024-02-08 22:23:29 -0600

    Chapter 14 Studio Completed

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,46 @@
 
 let launchcode = {
-  
+    organization : "nonprofit",
+    executiveDirector : "Jeff",
+    percentageCoolEmployees : 100,
+    programsOffered : ["Web Development", "Data Analysis", "Liftoff"],
+    launchOutput: function (num) {
+        let parts = {
+            2 : 'Launch!',
+            3 : 'Code!',
+            5 : 'Rocks!'
+        };
+        if (num % 2 === 'Launch!') {
+            return "Launch!";
+        } else if(num % 3 === 'Code!'){
+            return "Code!";
+        } else if(num % 5 === 'Rocks!') {
+            return "Rocks!"
+        }
+        let result = ""
+        for (let modulus in parts) {
+            if (num % Number(modulus) === 0) {
+                if (modulus == 5 && result.length > 0) {
+                    result += "";
+                }
+                result += parts[modulus];
+            }
+
+        }
+        if (result === "") {
+            result = "Rutabagas! that dosen't work.";
+        }
+        else {
+            result += "!";
+        }
+        if (result === "Launch Rocks!") {
+            result += "(CRASH!!!)";
+        }
+        return result;
+        
+    }
 }
+
 
 module.exports = launchcode;
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -4,5 +4,38 @@ const launchcode = require('../index.js');
 describe("Testing launchcode", function(){
 
   // Write your unit tests here!
-  
+  test("The key organization contains the value 'nonprofit'.", function(){
+    //let output = test.launchcode.organization;
+    expect(launchcode.organization).toBe("nonprofit");
+ });
+ test("The key executiveDirector contains the value 'Jeff'.", function(){
+  //let output = test.launchcode.executiveDirector;
+  expect(launchcode.executiveDirector).toBe("Jeff");    
+});
+test("The key percentageCoolEmployees contains the value 100.", function(){
+  //let output = test.launchcode.percentageCoolEmployees;
+  expect(launchcode.percentageCoolEmployees).toEqual(100);
+});
+test("The key programsOffered contains the value ['Web Development', 'Data Analysis', 'Liftoff'].", function(){
+  //let output = test.launchcode.programsOffered;
+  // expect(launchcode.programsOffered[0]).toBe('Web Development');
+  // expect(launchcode.programsOffered[1]).toBe('Data Analysis');
+  // expect(launchcode.programsOffered[2]).toBe('Liftoff');
+  expect(launchcode.programsOffered.length).toBe(3);
+});
+
+// nested describe block
+describe ("should have a method called launchOutput, which", function() {
+
+  test("should return 'Launch!' for numbers evenly divisible by 2", function() {
+    expect(launchcode.launchOutput(2)).toEqual('Launch!');
+  });
+  test("should return 'Code!' for numbers evenly divisible by 3", function() {
+    expect(launchcode.launchOutput(3)).toEqual('Code!');
+});
+  test("should return 'Rocks!' for numbers evenly divisible by 5", function() {
+    expect(launchcode.launchOutput(5)).toEqual('Rocks!');
+});
+ 
+});
 });
